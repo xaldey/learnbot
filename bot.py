9e9fb684e5f8d1640a91af990adedbd2c362128a
DenBell <xaldey@mail.ru>
2019-10-20 18:24:08 +0300

    Прикрутил фото и Эмоджи

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from random import choice
 
 from emoji import emojize
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 import settings
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -11,22 +12,31 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     filename='bot.log'
                     )
 
-def greet_user(bot, update):
-    emo = emojize(choice(settings.USER_EMOJI), use_aliases = True)
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_data['emo'] = emo
     text = 'Привет {}'.format(emo)
     logging.info(text)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
-    user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name, update.message.text)
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = "Привет {} {}! Ты написал: {}".format(update.message.chat.first_name, emo, update.message.text)
     logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
                 update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
-def send_rhino_picture(bot, update):
+def send_rhino_picture(bot, update, user_data):
     rhino_list = glob('images/rhino*.jp*g')
-    rhino_list = choice(rhino_list)
-    bot.send_photo(chat_id=update.message.chat.id, photo=open(rhino_list, 'rb'))
+    rhino_pic = choice(rhino_list)
+    bot.send_photo(chat_id=update.message.chat.id, photo=open(rhino_pic, 'rb'))
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else: 
+        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+        return user_data['emo']
 
 
 def main():
@@ -35,10 +45,10 @@ def main():
     logging.info('Бот запускается')
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(CommandHandler('rhino', send_rhino_picture))
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler('rhino', send_rhino_picture, pass_user_data=True))
 
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling()
     mybot.idle()
