af4c02059bcc491d33899aa1f17c1fc324000309
DenBell <xaldey@mail.ru>
2019-10-21 08:21:04 +0300

    Удалил из Гита файл настроек, лога

--- settings.py
+++
@@ -1,5 +0,0 @@
-PROXY = {'proxy_url': 'socks5://private.kilylabs.com:65506',
-        'urllib3_proxy_kwargs': {'username': 'teleproxy9','password': 'teleproxy9'}}
-API_KEY = "937043036:AAGa4yAM0YMEcty5qLN8QF2dTWYZj2c9Tx8" 
-
-USER_EMOJI = [':smiley_cat:', ':smiling_imp:',':panda_face:',':dog:']
